a6f3f1180cf1d47f92b74692be14f67e082ce81e
Mihir Tiwari <mtiw999@gmail.com>
2017-02-25 16:59:36 -0500

    comments

--- app.py
+++ app.py
@@ -12,8 +12,8 @@ def get_drugs_taken(firstName, lastName):
     drug = api.get_drug(firstName, lastName)
     return jsonify({'drug': drug})
 
-@app.route('/combine', methods=['POST'])
-def combined_drugs():
+# @app.route('/combine', methods=['POST'])
+# def combined_drugs():
 
 
 if __name__ == "__main__":
