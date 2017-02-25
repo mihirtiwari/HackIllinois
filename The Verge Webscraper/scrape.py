caa37d7899f539eed0c7f54e9c141eca3fb97980
Pravin Sivabalan <pravinsivabalan@gmail.com>
2017-02-25 10:25:29 -0600

    Add webscraping example

---
+++ The Verge Webscraper/scrape.py
@@ -0,0 +1,15 @@
+import urllib2
+import tweepy
+
+page = urllib2.urlopen("http://www.theverge.com")
+html = page.read()
+
+beg = html.index('<div class="c-masthead__tagline">')
+end = html.index("</div>", beg + 1)
+
+parsed = html[beg:end]
+
+tagBeg = parsed.index('">', parsed.index("<a href="))
+tagEnd = parsed.index("</a>")
+
+tagline = parsed[tagBeg + 2:tagEnd]
