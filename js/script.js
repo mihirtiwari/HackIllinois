76be5adfd80d0d55181887b8449026488369ceb3
Pravin Sivabalan <pravinsivabalan@gmail.com>
2017-02-25 15:53:55 -0600

    Add js code to access api

--- js/script.js
+++ js/script.js
@@ -1,29 +1,29 @@
 function login() {
   var firstName = document.getElementById("firstName").value;
   firstName = firstName.replace(/\s+/g, '');
+
   var lastName = document.getElementById("lastName").value
   lastName = lastName.replace(/\s+/g, '');
 
-  var xhr = new XMLHttpRequest();
-  xhr.open("POST", '/server', true);
+  var xhttp = new XMLHttpRequest();
+  xhttp.open("GET", "http://127.0.0.1:5000/taken/Jason?Argonauts", false);
+  xhttp.setRequestHeader("Content-type", "application/json");
+  xhttp.send();
+  var response = JSON.parse(xhttp.responseText);
 
-  xhr.onreadystatechange = function() {
-      if(xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200) {
-          // Request finished. Do processing here.
-      }
-  }
+  console.log(response);
 
   saveName(firstName, lastName);
   document.location.href = 'index.html';
 }
 
 function saveName(firstName, lastName) {
- var name = {
+  var name = {
    First: firstName,
    Last: lastName
- };
+  };
 
- name = JSON.stringify(name);
- name = btoa(name);
- localStorage.setItem('_name', name);
+  name = JSON.stringify(name);
+  name = btoa(name);
+  localStorage.setItem('_name', name);
 }
