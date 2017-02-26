d32425fe8066eaef419c044de2a26dbf49c95f9f
Pravin Sivabalan <pravinsivabalan@gmail.com>
2017-02-26 00:01:10 -0600

    Add some stuff

--- js/script.js
+++ js/script.js
@@ -18,8 +18,15 @@ function saveJSON(json) {
 function generateCard(drug){
   var title = drug.substring(drug.indexOf("(") + 1, drug.indexOf(")"));
   var description = drug;
-  var card = "<div style=\"margin-top:20px; margin-right: 40px;\" class=\"card\"> <div class=\"card-block\"> <h3 class=\"card-title\">" + title + "</h3> <p class=\"card-text\">" + description + "</p></div></div>";
+  var card = "<div style=\"margin-top:20px; margin-right: 40px;\" class=\"card\"><div class=\"card-header\" style=\"background-color:#515151;color:white;\">Featured</div><div class=\"card-block\"> <h3 class=\"card-title\">" + title + "</h3> <p class=\"card-text\">" + description + "</p></div></div>";
 
   var div = document.getElementById("drugs");
   div.innerHTML = div.innerHTML + card;
+}
+
+function generateAlert(alertMessage){
+  var alert = "<div class=\"alert alert-danger\" role=\"alert\" style=\"margin-top:50px; margin-left:10px;\"><strong>Careful! </strong>" + alertMessage + "</div>";
+
+  var div = document.getElementById("alerts")
+  div.innerHTML = div.innerHTML + alert;
 }
